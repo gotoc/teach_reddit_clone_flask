2fc27be671c5165d5eee81eb5535dbff55e050ce
Shahid Chohan <schohan.15@ucla.edu>
2015-01-15 03:46:22 -0800

    Fix field alignment style in models.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from app import db
 
 class Post(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(255))
-  url = db.Column(db.String(255))
+  id        = db.Column(db.Integer, primary_key=True)
+  title     = db.Column(db.String(255))
+  url       = db.Column(db.String(255))
   votecount = db.Column(db.Integer, default=0)
   subreddit = db.Column(db.String(255))
 
@@ -11,9 +11,9 @@ class Post(db.Model):
     return '<Post %r: %s>' % (self.title, self.subreddit)
 
 class Comment(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
+  id      = db.Column(db.Integer, primary_key=True)
   post_id = db.Column(db.Integer)
-  text = db.Column(db.String(255))
+  text    = db.Column(db.String(255))
 
   def __repr__(self):
     return '<Comment %r: %s>' % (self.text, self.post_id)
